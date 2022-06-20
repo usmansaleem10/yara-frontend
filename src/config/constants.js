f20c8c140d3e8203d94f727f9af110f5ff59d8c7
usman saleem <usman@hivebuy.de>
2022-06-20 18:10:00 +0500

    fix: fix backend url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://yara-backend-app.herokuapp.com";
+export const BASE_URL = "https://yara-backend-app.herokuapp.com";
 
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
